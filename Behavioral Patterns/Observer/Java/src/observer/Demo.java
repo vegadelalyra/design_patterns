de13a404e4a47a8528b740cd72f80324b33764d7
Vegadelalyra <lyradeperseo@gmail.com>
2023-11-01 01:16:56 -0500

    feat: implement observer in java

---
+++ Behavioral Patterns/Observer/Java/src/observer/Demo.java
@@ -0,0 +1,19 @@
+package observer;
+
+import observer.editor.Editor;
+import observer.listeners.EmailNotificationListener;
+import observer.listeners.LogOpenListener;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("open", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }    
+}
---
+++ Behavioral Patterns/Observer/Java/src/observer/listeners/EventListener.java
@@ -0,0 +1,7 @@
+package observer.listeners;
+
+import java.io.File;
+
+public interface EventListener {
+   void update(String eventType, File file);
+}
